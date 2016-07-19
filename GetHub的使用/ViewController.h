578dc3aaf7db3bd5b3f54a7ac54bb6bd856400e3
d1php <1264786803@qq.com>
2016-07-19 16:11:52 +0800

    Initial Commit

---
+++ GetHub的使用/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GetHub的使用
+//
+//  Created by 王文志 on 16/7/19.
+//  Copyright © 2016年 王文志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GetHub的使用/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GetHub的使用
+//
+//  Created by 王文志 on 16/7/19.
+//  Copyright © 2016年 王文志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
